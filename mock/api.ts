891da2144da9359f1b16100eb6eba86350dac966
Deansquirrel <yuansong6@163.com>
2020-03-14 18:07:20 +0800

    登录界面

--- mock/api.ts
+++ mock/api.ts
@@ -7,4 +7,21 @@ export default {
     'GET /api/tags': mockjs.mock({
         'list|100': [{ name: '@city', 'value|1-100': 50, 'type|0-2': 1 }],
     }),
+    'POST /api/login': (req: any, res: any) => {
+        const data = req.body;
+        setTimeout(() => {
+            if (data.username == "yuansong" && data.password == "yuansong") {
+                res.json({
+                    code: 0,
+                    msg: "success"
+                });
+            } else {
+                res.json({
+                    code: -1,
+                    msg: "login failed"
+                });
+            }
+
+        }, 3000);
+    }
 }
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,16 +1,123 @@
 import React from 'react';
 import { Link } from 'umi';
 import styles from './login.less';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-import constant from "@/component/constant";
+// import constant from "@/component/constant";
+import { Form, Input, Button } from 'antd';
+
+const LoginForm = () => {
+
+  const [form] = Form.useForm();
+
+  return (
+    <Form
+      className={styles.login_form}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input you username"
+          }
+        ]}
+      >
+        <Input size={"large"}
+          prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="Username"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input you password"
+          }
+        ]}
+      >
+        <Input size={"large"}
+          prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25' }} />}
+          placeholder="Password"
+          type="password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button
+          // loading:isLoading
+          loading={false}
+          type="primary"
+          style={{ width: '100%', marginTop: 30 }}
+          size={"large"}
+          htmlType={"submit"}
+        >
+          Log in
+        </Button>
+      </Form.Item>
+    </Form >
+  )
+}
+
+const onFinish = (values: any) => {
+  console.log("success", values);
+  // isLoading = true;
+  const checkResult = checkLogin(values.username, values.password);
+  if (checkResult) {
+    console.log("check login success");
+  } else {
+    console.log("check login failed");
+  }
+  // isLoading = false;
+}
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed", errorInfo);
+}
+
+const checkLogin = async (username: string, password: string) => {
+  console.log("checkLogin");
+  console.log(username, password);
+  fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password
+    })
+  })
+    .then(response => response.json())
+    .then(function (data) {
+      if (data.code == 0) {
+        return true;
+      } else {
+        if (data.msg != undefined) {
+          console.log(data.msg);
+        }
+        return false;
+      }
+    })
+    .catch(function (e) {
+      console.log(e);
+      return false;
+    });
+}
 
 export default () => {
   return (
-    <div className={styles.login_form}>
+    <div className={styles.div_login_form}>
       <div>
-        <h1 className={styles.title}>Page login {constant.version}</h1>
+        <h1 className={styles.title}>Login</h1>
       </div>
+      <LoginForm />
       <Link to="/management/welcome">to Welcome</Link>
+      {/* <div className={styles.cp}>
+        {constant.copyright}
+      </div> */}
     </div>
   );
 }
