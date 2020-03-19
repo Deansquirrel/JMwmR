6695cd67687b6742e640cec3c348755cbcf8f993
Deansquirrel <yuansong6@163.com>
2020-03-19 23:35:38 +0800

    1、增加svg组件，自定义图标（根据网络搜素svg文件） 2、完善主页导航页面跳转

--- src/layouts/managementLayouts.tsx
+++ src/layouts/managementLayouts.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { HomeIcon } from "@/component/svg";
+
+import { history } from 'umi';
+
+// import { Link } from 'umi';
 
 import styles from './managementLayouts.less';
 const { Header, Footer, Sider, Content } = Layout;
-import { MenuUnfoldOutlined, MenuFoldOutlined, UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+const { SubMenu } = Menu;
+import { MailOutlined, MenuUnfoldOutlined, MenuFoldOutlined, AppstoreOutlined, SettingOutlined, PieChartOutlined } from '@ant-design/icons';
 
 import constant from "@/component/constant";
-import { BasicProps } from 'antd/lib/layout/layout';
 import { SiderProps } from 'antd/lib/layout/Sider';
 
 declare interface IState {
@@ -72,23 +77,59 @@ class ManagementLayouts extends React.Component<{}, IState> {
                     collapsed={this.state.collapsed}
                 >
                     <div className={styles.logo} />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                        <Menu.Item key="1">
-                            <UserOutlined />
-                            <span className="nav-text">nav 1</span>
+                    <Menu
+                        defaultOpenKeys={['sub1']}
+                        theme={"dark"}
+                        mode={"inline"}
+                    >
+                        <Menu.Item key="welcome" onClick={() => { history.push('/management/welcome'); }}>
+                            <HomeIcon />
+                            <span>Welcome</span>
                         </Menu.Item>
-                        <Menu.Item key="2">
-                            <VideoCameraOutlined />
-                            <span className="nav-text">nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <UploadOutlined />
-                            <span className="nav-text">nav 3</span>
-                        </Menu.Item>
-                        <Menu.Item key="4">
-                            <UserOutlined />
-                            <span className="nav-text">nav 4</span>
-                        </Menu.Item>
+                        <SubMenu
+                            key="sub1"
+                            title={
+                                <span>
+                                    <MailOutlined />
+                                    <span>Navigation One</span>
+                                </span>
+                            }
+                        >
+                            <Menu.Item key="1" onClick={() => { history.push('/management/hello'); }}>Option 1</Menu.Item>
+                            <Menu.Item key="2">Option 2</Menu.Item>
+                            <Menu.Item key="3">Option 3</Menu.Item>
+                            <Menu.Item key="4">Option 4</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub2"
+                            title={
+                                <span>
+                                    <AppstoreOutlined />
+                                    <span>Navigation Two</span>
+                                </span>
+                            }
+                        >
+                            <Menu.Item key="5">Option 5</Menu.Item>
+                            <Menu.Item key="6">Option 6</Menu.Item>
+                            <SubMenu key="sub3" title="Submenu">
+                                <Menu.Item key="7">Option 7</Menu.Item>
+                                <Menu.Item key="8">Option 8</Menu.Item>
+                            </SubMenu>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub4"
+                            title={
+                                <span>
+                                    <SettingOutlined />
+                                    <span>Navigation Three</span>
+                                </span>
+                            }
+                        >
+                            <Menu.Item key="9">Option 9</Menu.Item>
+                            <Menu.Item key="10">Option 10</Menu.Item>
+                            <Menu.Item key="11">Option 11</Menu.Item>
+                            <Menu.Item key="12">Option 12</Menu.Item>
+                        </SubMenu>
                     </Menu>
                 </Sider>
                 <Layout style={{ marginLeft: silderWidth, minHeight: "100vh" }}>
@@ -99,12 +140,6 @@ class ManagementLayouts extends React.Component<{}, IState> {
                                 (<MenuFoldOutlined className={styles.trigger} onClick={() => this.changeCollapsed()} />)
 
                         }
-                        {/* {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                            className: { { styles.trigger }},
-                            onClick: () => {
-                            console.log("click");
-                            },
-                        })} */}
                     </Header>
                     <Content className={styles.content}>{this.props.children}</Content>
                     <Footer style={{ textAlign: "center" }}><span className={styles.footer}>{constant.copyright}</span></Footer>
@@ -115,59 +150,3 @@ class ManagementLayouts extends React.Component<{}, IState> {
 }
 
 export default ManagementLayouts;
-
-// export default (props: any) => {
-//     return (
-
-//         <Layout>
-//             <Sider
-//                 breakpoint="lg"
-//                 // collapsedWidth="0"
-//                 onBreakpoint={broken => {
-//                     console.log(broken);
-//                 }}
-//                 onCollapse={(collapsed, type) => {
-//                     console.log(collapsed, type);
-//                 }}
-//                 zeroWidthTriggerStyle={{
-//                     marginLeft: 200
-//                 }}
-//             // style={{
-//             //     overflow: 'auto',
-//             //     height: '100vh',
-//             //     position: 'fixed',
-//             //     left: 0,
-//             // }}
-//             >
-//                 <div className="logo" />
-//                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-//                     <Menu.Item key="1">
-//                         <UserOutlined />
-//                         <span className="nav-text">nav 1</span>
-//                     </Menu.Item>
-//                     <Menu.Item key="2">
-//                         <VideoCameraOutlined />
-//                         <span className="nav-text">nav 2</span>
-//                     </Menu.Item>
-//                     <Menu.Item key="3">
-//                         <UploadOutlined />
-//                         <span className="nav-text">nav 3</span>
-//                     </Menu.Item>
-//                     <Menu.Item key="4">
-//                         <UserOutlined />
-//                         <span className="nav-text">nav 4</span>
-//                     </Menu.Item>
-//                 </Menu>
-//             </Sider>
-//             <Layout>
-//                 <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-//                 <Content style={{ margin: '24px 16px 0' }}>
-//                     <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-//                         content
-//         </div>
-//                 </Content>
-//                 <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-//             </Layout>
-//         </Layout>
-//     )
-// }
