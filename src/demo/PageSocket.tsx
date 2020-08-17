effcdae81d59256ca1f5daa7c79c92ca899fcb82
yuansong6@163.com <yuansong6@163.com>
2020-08-17 18:42:18 +0800

    ls

--- src/demo/PageSocket.tsx
+++ src/demo/PageSocket.tsx
@@ -23,10 +23,24 @@ class PageSocket extends BaseComponent<{}, IState> {
     if (super.componentDidMount != undefined) {
       super.componentDidMount();
     }
+
+    //获取当前主机路径
+    console.log(window.location.host);
+
+    console.log(typeof WebSocket);
+    /**
+     * 检查浏览器是否支持WebSocket
+     */
+    if (typeof WebSocket != undefined) {
+      console.log('1');
+    } else {
+      console.log('2');
+    }
+
     this.ws = new WebSocket('ws://192.168.8.99:8090');
     this.ws.onopen = () => {
       console.log('send data');
-      this.ws.send('send data');
+      // this.ws.send('send data');
     };
 
     this.ws.onmessage = evt => {
